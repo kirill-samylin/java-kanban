13f2002633ddc556725f6e8c743ab9898ca5207f
ksamylin <kirill-itb@yandex.ri>
2025-03-19 16:39:36 +0300

    fix: review comments

--- test/HttpPrioritizedHandledTest.java
+++ test/HttpPrioritizedHandledTest.java
@@ -1,27 +1,13 @@
-import app.entities.Epic;
 import app.entities.SubTask;
-import app.entities.Task;
-import app.enums.TaskStatus;
 import app.exceptions.TaskOverlapException;
-import app.http.HttpTaskServer;
-import app.interfaces.TaskManager;
-import app.utils.Managers;
-import com.google.gson.Gson;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.net.URI;
-import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class HttpPrioritizedHandledTest extends HttpBaseHandlerTest {
     public HttpPrioritizedHandledTest() throws IOException {
